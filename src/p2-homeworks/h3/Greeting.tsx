b0bc566894f56aa800b5819101188e3337913933
AnaKhurs <ana.khurs@gmail.com>
2021-10-31 01:36:31 +0300

    HW3_css

--- src/p2-homeworks/h3/Greeting.tsx
+++ src/p2-homeworks/h3/Greeting.tsx
@@ -14,20 +14,22 @@ type GreetingPropsType = {
 const Greeting: React.FC<GreetingPropsType> = (
     {name, setNameCallback, addUser, error, totalUsers, onEnter} // деструктуризация пропсов
 ) => {
-    const inputClass = s.error // need to fix with (?:)
+    const inputClass = error ? s.errorInput : s.input
 
     return (
-        <div>
-            <input
-                value={name}
-                onChange={setNameCallback}
-                className={inputClass}
-                onKeyPress={onEnter}
-                onBlur={setNameCallback}
-            />
-            <div>{error}</div>
-            <button onClick={addUser} disabled={!name}>add</button>
-            <div>{totalUsers}</div>
+        <div className={s.greeting}>
+            <div>
+                <input
+                    value={name}
+                    onChange={setNameCallback}
+                    className={inputClass}
+                    onKeyPress={onEnter}
+                    onBlur={setNameCallback}
+                />
+                <div className={s.errorMessage}>{error}</div>
+            </div>
+            <button className={s.button} onClick={addUser} disabled={!name}>add</button>
+            <div className={s.totalUsers}>{totalUsers}</div>
         </div>
     )
 }
