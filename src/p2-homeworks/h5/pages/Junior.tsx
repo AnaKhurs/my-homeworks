7566b0b0ba7e829d274bb75ae847d670ee79e3c2
AnaKhurs <ana.khurs@gmail.com>
2021-11-28 23:01:42 +0300

    Update Junior.tsx

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import HW7 from "../../h7/HW7";
 
 export function Junior() {
     return (
         <div>
-            Junior
+            <HW7/>
         </div>
     )
 }
