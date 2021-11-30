690020980afe7936c8b42735632dc4b186d3bd3a
AnaKhurs <ana.khurs@gmail.com>
2021-11-30 23:09:15 +0300

    Update SuperRadio.tsx

--- src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
+++ src/p2-homeworks/h7/common/c6-SuperRadio/SuperRadio.tsx
@@ -16,7 +16,8 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
     }
 ) => {
     const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
-        // onChange, onChangeOption
+        onChange && onChange(e)
+        onChangeOption && onChangeOption(e.currentTarget.value)
     }
 
 
@@ -24,6 +25,10 @@ const SuperRadio: React.FC<SuperRadioPropsType> = (
         <label key={name + '-' + i}>
             <input
                 type={'radio'}
+                name={name}
+                checked={o === value}
+                value={o}
+                onChange={onChangeCallback}
                 // name, checked, value, onChange
             />
             {o}
